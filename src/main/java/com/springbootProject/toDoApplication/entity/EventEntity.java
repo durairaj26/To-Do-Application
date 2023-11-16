2812fee9527aca5a64ea98b9888cf474f0acd7cd
durairaj26 <durairajdesingan@gmail.com>
2023-11-16 12:04:26 +0530

    Created and Updated entity package, EventEntity.java class and Updated application.properties file

---
+++ src/main/java/com/springbootProject/toDoApplication/entity/EventEntity.java
@@ -0,0 +1,68 @@
+package com.springbootProject.toDoApplication.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="events")
+public class EventEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String description;
+	private LocalDateTime dateandtime;
+	private String status="Pending";
+	public EventEntity(Long id, String name, String description, LocalDateTime dateandtime, String status) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.dateandtime = dateandtime;
+		this.status = status;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public LocalDateTime getdateandtime() {
+		return dateandtime;
+	}
+	public void setdateandtime(LocalDateTime dateandtime) {
+		this.dateandtime = dateandtime;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	@Override
+	public String toString() {
+		return "EventEntity [id=" + id + ", name=" + name + ", description=" + description + ", dateandtime=" + dateandtime
+				+ ", status=" + status + "]";
+	}
+	
+	
+
+}
