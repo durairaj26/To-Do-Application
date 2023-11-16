796fcf8570d3bfe5e7383529c1b5a7ae5505f8d0
durairaj26 <durairajdesingan@gmail.com>
2023-11-16 11:10:51 +0530

    Created To-Do application
    The spring boot application created from spring initializr and pushed to feature branch.

---
+++ src/test/java/com/springbootProject/toDoApplication/ToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootProject.toDoApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
