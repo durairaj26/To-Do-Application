ac1b1eeb7863f2ef0a95321c741959b9bbb160e9
durairaj26 <durairajdesingan@gmail.com>
2023-11-17 17:48:02 +0530

    Updated List for findEventByStatus

--- src/main/java/com/springbootProject/toDoApplication/repository/EventRepository.java
+++ src/main/java/com/springbootProject/toDoApplication/repository/EventRepository.java
@@ -1,5 +1,7 @@
 package com.springbootProject.toDoApplication.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import com.springbootProject.toDoApplication.entity.EventEntity;
 
 @Repository
 public interface EventRepository extends JpaRepository<EventEntity, Long> {
+
+	List<EventEntity> findEventByStatus(String status);
+
 
 }
