03770481c32414294f1e58e3ad769eb002e88e40
durairaj26 <durairajdesingan@gmail.com>
2023-11-20 16:40:46 +0530

    Updated deleteEvent()
    Remove if condition

--- src/main/java/com/springbootProject/toDoApplication/services/EventServices.java
+++ src/main/java/com/springbootProject/toDoApplication/services/EventServices.java
@@ -1,66 +1,67 @@
 package com.springbootProject.toDoApplication.services;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.springbootProject.toDoApplication.entity.EventEntity;
 import com.springbootProject.toDoApplication.repository.EventRepository;
 
 @Service
 public class EventServices {
-	
+
 	@Autowired
 	private EventRepository eventRepository;
-	
+
 	public EventEntity createEvent(EventEntity eventEntity) {
 		return eventRepository.save(eventEntity);
 	}
-	
-	public List<EventEntity> getAllEvent(){
+
+	public List<EventEntity> getAllEvent() {
 		return eventRepository.findAll();
-		
 	}
-	
+
 	public EventEntity updateEvent(EventEntity eventEntity, Long id) {
-			EventEntity updateEventEntity= eventRepository.findById(id).orElseThrow(()->new RuntimeException("Event not found"));
-			updateEventEntity.setName(eventEntity.getName());
-			updateEventEntity.setDescription(eventEntity.getDescription());
-			updateEventEntity.setdateandtime(eventEntity.getdateandtime());
-			return eventRepository.save(updateEventEntity);
+		EventEntity updateEventEntity = eventRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Event not found"));
+		updateEventEntity.setName(eventEntity.getName());
+		updateEventEntity.setDescription(eventEntity.getDescription());
+		updateEventEntity.setdateandtime(eventEntity.getdateandtime());
+		return eventRepository.save(updateEventEntity);
+
+	}
+
+	public void deleteEvent(Long id) {
+		eventRepository.findById(id).orElseThrow(() -> new RuntimeException("Event not found"));
+			eventRepository.deleteById(id);
 		
 	}
 
-		
-	public void deleteEvent(Long id) {
-		EventEntity findEventId= eventRepository.findById(id).orElseThrow(()-> new RuntimeException("Event not found"));
-		if(findEventId.equals(id)) {
-			eventRepository.deleteById(id);
-		}
-	}
-	
-	
 	public void changeStatusAsCompleted(Long id) {
-		EventEntity findEventId= eventRepository.findById(id).orElseThrow(()-> new RuntimeException("Event not found"));
+
+		EventEntity findEventId = eventRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Event not found"));
 		findEventId.setStatus("Completed");
 		eventRepository.save(findEventId);
 	}
-	
-	
-	public List<EventEntity> groupEventByStatus(String status) {
-		return eventRepository.findEventByStatus(status);
+
+
+	public Map<String, List<EventEntity>> getGroup() {
+	    List<EventEntity> overdueEvents = eventRepository.findAll().stream()
+	            .filter(event -> "Overdue".equals(event.isOverdue()))
+	            .peek(event -> event.setStatus("Overdue"))
+	            .collect(Collectors.toList());
+
+	    Map<String, List<EventEntity>> groupByStatus = eventRepository.findAll().stream()
+	            .collect(Collectors.groupingBy(EventEntity::getStatus));
+
+	    groupByStatus.put("Overdue", overdueEvents);
+
+	    return groupByStatus;
 	}
-	
-	
-//	 public List<EventEntity> getGroupEventByStatus(){
-//		  List<EventEntity> pendingEvents = groupEventByStatus("Pending");
-//		  List<EventEntity> completedEvents = groupEventByStatus("Pending");
-//		  List<EventEntity> overdueEvent = eventRepository.findAll().stream()
-//	                .filter(t->!"Completed".equals(t.getStatus()) && LocalDateTime.now().isAfter(t.getdateandtime()))
-//	                .map(EventEntity).
-//	                .toList();
-//		  pendingEvents.addAll(overdueEvent);
-//		  pendingEvents.addAll(completedEvents);
-//		  return overdueEvent;
-//	 }
 
 }
